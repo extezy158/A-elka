40245cb048a734e2472d3d1290cb4a46728426b2
extezy158 <exxxtttezy@gmail.com>
2025-11-19 18:52:04 +0300

    jghghghgh
    rfrfrfrfrfrfrfrfrfr

---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+console.log("hello");
+
+let best=['gn','fs','sa','das','asd'];
+
+let numbers=[10,20,30];
+for(let i=0;i<numbers.length;i++){
+    console.log(numbers[i]);
+}
+
+let fruits=['apple','banana','orange'];
+for(let fruit of fruits){
+    console.log(fruit);
+}
+
+let students=[
+    {name:'John',age:20},
+    {name:'Jane',age:18},
+    {name:'Bob',age:22},
+];
+
+for(let student of students){
+    console.log(student.name);
+}
+
+let pets=['dog','cat','fish']
+console.log(pets.include('dog'))
+console.log(pets.include('рыба'))
+let nums1=[10,20,30,40];
+
+let part=nums1.slice(1,3);
+console.log(part);
+console.log(nums1);
+
+let arr=[1 ]
+
